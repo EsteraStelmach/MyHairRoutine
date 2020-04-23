3d4299aa28db6d7ce785da762b3352763288d13f
Estera Stelmach <esterkastel@gmail.com>
2020-04-23 13:26:51 +0200

    Adding Resource Bundles:en,pl,nl

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -14,10 +17,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Stage stage = new Stage();
+       // Locale.setDefault(new Locale("pl"));
+        // Locale.setDefault(new Locale("nl"));
+        ResourceBundle bundle = ResourceBundle.getBundle("Bundles.messages");
 
         FXMLLoader fxmlLoader=new FXMLLoader();
         fxmlLoader.setLocation(this.getClass().getResource("/fxml/LoginWindow.fxml"));
+        fxmlLoader.setResources(bundle);
         Parent loginWindowBorderPane = fxmlLoader.load();
+
 
         Scene scene = new Scene(loginWindowBorderPane);
         stage.setScene(scene);
